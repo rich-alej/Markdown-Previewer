ad04300393b7b23d0b600de168aeae911f9a42cc
rich-alej <torresalex0907@gmail.com>
2023-08-10 07:23:41 -0400

    Se inicia el proyecto

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import Test from './components/Test';
+
+ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<Test />, document.getElementById('test'))
